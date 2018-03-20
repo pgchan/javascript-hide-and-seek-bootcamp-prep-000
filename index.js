3e0be129af6b7f3ce55b9a55499a250469ad6844
P G <pgauchan@gmail.com>
2018-03-20 02:18:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.querySelector("#nested");
+}
+
+function increaseRankBy(n){
+  const lis = document.querySelectorAll("ul.ranked-list li");
+  for ( let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+  }
+}
+
+
+function deepestChild(){
+  var divs = document.querySelectorAll("#grand-node");
+  var items = divs[0].querySelectorAll('div');
+  return items[items.length-1];
+}
