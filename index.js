680bd9e0c09cf313cc0ad49495b690fada27c065
P G <pgauchan@gmail.com>
2018-03-20 02:14:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function nestedTarget(){
 function increaseRankBy(n){
   const lis = document.querySelectorAll("ul.ranked-list li");
   for ( let i = 0; i < lis.length; i++) {
-    lis[i].innerHTML = parseInt(newRank[i].innerHTML) + n;
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
   }
 }
